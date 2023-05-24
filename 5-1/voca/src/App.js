86b3a508ad161eef942ab35ee1d26868dc458f14
hotdog1004 <hotdog87675210@gmail.com>
2023-05-24 17:15:40 +0900

    Feat : 신규 페이지 이동을 위해 Route와 Link 추가

--- 5-1/voca/src/App.js
+++ 5-1/voca/src/App.js
@@ -1,3 +1,5 @@
+import CreateDay from "./component/CreateDay";
+import CreateWord from "./component/CreateWord";
 import Day from "./component/Day";
 import DayList from "./component/DayList";
 import EmptyPage from "./component/EmptyPage";
@@ -11,8 +13,9 @@ export default function App() {
         <Header />
         <Routes>
           <Route path="/" element={<DayList />} />
-
           <Route path="/day/:day" element={<Day />} />
+          <Route path="/create_word" element={<CreateWord />} />
+          <Route path="/create_day" element={<CreateDay />} />
           <Route path="*" element={<EmptyPage />} />
         </Routes>
       </div>
