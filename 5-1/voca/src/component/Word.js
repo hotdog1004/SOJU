179354ed992bae762881fae51d7a0b2f1445d671
hotdog1004 <hotdog87675210@gmail.com>
2023-05-24 16:06:18 +0900

    Feat : 단어 수정과 삭제 기능 구현

--- 5-1/voca/src/component/Word.js
+++ 5-1/voca/src/component/Word.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
-export default function Word({ word }) {
+export default function Word({ word: w }) {
+  const [word, setWord] = useState(w);
   const [isShow, setIsShow] = useState(false);
   // 처음엔 뜻이 안 보여야하니 초기값을 false로 준다.
   const [isDone, setIsDone] = useState(word.isDone);
@@ -8,8 +9,39 @@ export default function Word({ word }) {
     setIsShow(!isShow);
   };
   const toggleDone = () => {
-    setIsDone(!isDone);
+    // setIsDone(!isDone);
+    // isDone 부분을 기억할 수 있도록 처리해보자
+    fetch(`http://localhost:3001/words/${word.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...word,
+        isDone: !isDone,
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        setIsDone(!isDone);
+      }
+    });
   };
+
+  const del = () => {
+    if (window.confirm("삭제하시겠습니까?")) {
+      fetch(`http://localhost:3001/words/${word.id}`, {
+        method: "DELETE",
+      }).then((res) => {
+        if (res.ok) {
+          // 삭제가 잘 처리 됐으면 word의 id를 0으로 세팅한다.
+          setWord({ id: 0 });
+        }
+      });
+    }
+  };
+
+  if (word.id === 0) return null;
+
   return (
     <tr className={isDone ? "off" : ""}>
       <td>
@@ -19,7 +51,9 @@ export default function Word({ word }) {
       <td>{isShow && word.kor}</td>
       <td>
         <button onClick={toggleShow}>뜻 {isShow ? "숨기기" : "보기"} </button>
-        <button className="btn_del">삭제 </button>
+        <button onClick={del} className="btn_del">
+          삭제
+        </button>
       </td>
     </tr>
   );
